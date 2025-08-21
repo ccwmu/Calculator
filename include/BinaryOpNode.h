5c1a0251162346b980264d155385ddbd07b5a78d
Ethan Ye <147452670+ccwmu@users.noreply.github.com>
2025-08-21 16:52:17 -0700

    added some comments

--- include/BinaryOpNode.h
+++ include/BinaryOpNode.h
@@ -1,3 +1,14 @@
+/** 
+ * @file BinaryOpNode.h
+ * @brief Binary operation nodes for the AST
+ * @author Ethan Ye
+ * @date 2025-8-20
+ * 
+ * This header defines all binary (two-operand) mathematical operation
+ * nodes for the AST. Operations are addition, subtraction, multiplication,
+ * division, and exponentiation.
+ */
+
 #ifndef BINARYOPNODE_H
 #define BINARYOPNODE_H
 
@@ -7,21 +18,41 @@
 
 using namespace std;
 
-// List of operations: 
-// Add, Subtract, Multiply, Divide, Power
+/**
+ * @class AddNode
+ * @brief Node representing an addition operation
+ *
+ * Evaluates the sum of two child nodes
+ */
 
 class AddNode : public Node {
 private: 
-    unique_ptr<Node> child1;
-    unique_ptr<Node> child2;
+    unique_ptr<Node> child1; /// < Left operand
+    unique_ptr<Node> child2; /// < Right operand
 
 public: 
+
+    /**
+     * @brief Construct a new AddNode
+     * @param left Left operand
+     * @param right Right operand
+     */
     AddNode(unique_ptr<Node> left, unique_ptr<Node> right)
         : child1(move(left)), child2(move(right)) {}
 
+    /**
+     * @brief Evaluate the addition operation
+     * @param variables Map of variable names to values
+     * @return Sum of left and right nodes
+     */
     long double evaluate(const map<string, long double>& variables) const override {
         return child1->evaluate(variables) + child2->evaluate(variables);
     }
+
+    /**
+     * @brief Creates a deep copy of the node
+     * @return Pointer to a cloned new AddNode
+     */
     Node* clone() const override {
         return new AddNode(unique_ptr<Node>(child1->clone()), unique_ptr<Node>(child2->clone()));
     }
